f07f42d17fa9c426ff695179d7d62425cfd4f1fd
thetonymaster <antonio.cabreraglz@gmail.com>
2016-03-16 13:00:47 -0600

    Add User route to React Router

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,11 @@ import { Route, IndexRoute } from 'react-router';
 
 import App from './components/App';
 import ServiceForm from './components/ServiceForm';
+import UserProfile from './components/UserProfile';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={ServiceForm} />
+  <Route path="user" component={UserProfile} />
   </Route>
 );
