f4d2bfc258542ebe6a08f597856e06502154fb80
thetonymaster <antonio.cabreraglz@gmail.com>
2016-03-16 15:13:39 -0600

    Add AccountSidebar component

---
+++ src/components/user/AccountSiderbar.js
@@ -0,0 +1,19 @@
+import React, { PropTypes } from 'react';
+import { Link } from 'react-router';
+
+const AccountSidebar = () => {
+  return (
+      <div>
+        <nav>
+          <ul>
+            <li><Link to="#">Account</Link></li>
+            <li><Link to="#">Billing</Link></li>
+            <li><Link to="#">Team</Link></li>
+            <li><Link to="#">Project/Services</Link></li>
+          </ul>
+        </nav>
+      </div>
+  );
+};
+
+export default AccountSidebar;
