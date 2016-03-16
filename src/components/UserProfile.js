f69e2332cf60a85dd57ba1ef19e4d3b975e83419
thetonymaster <antonio.cabreraglz@gmail.com>
2016-03-16 14:13:47 -0600

    Add Profile Component

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,9 +1,11 @@
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
+import Profile from './user/Profile';
 
 const UserProfile = () => {
     return (
         <div>
+          <Profile />
         </div>
     );
 };
---
+++ src/components/user/Profile.js
@@ -0,0 +1,30 @@
+import React, { PropTypes } from 'react';
+import { Link } from 'react-router';
+
+const Profile = () => {
+    return (
+      <div>
+        <h2>Profile</h2>
+        <img src="http://lorempixel.com/g/200/200/" />
+        <h3>Username</h3>
+        <div>
+          <ul>
+            <li>
+              <span>email@host.com</span>
+            </li>
+            <li>
+              <span>Address</span>
+            </li>
+            <li>
+              <Link to={'/user/id'}>Add/Edit Address</Link>
+            </li>
+            <li>
+              <Link to={'/user/id'}>Edit</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+};
+
+export default Profile;
