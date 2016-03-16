969c5f574e014530516c685af6b2135374b70530
thetonymaster <antonio.cabreraglz@gmail.com>
2016-03-16 14:14:06 -0600

    Add UserProfile to router

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,6 @@ import UserProfile from './components/UserProfile';
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={ServiceForm} />
-  <Route path="user" component={UserProfile} />
+    <Route path="user" component={UserProfile} />
   </Route>
 );
