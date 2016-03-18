9e75ec3e89f8b5f7fd6938a020bba6b8e7b2080b
thetonymaster <antonio.cabreraglz@gmail.com>
2016-03-18 15:31:28 -0600

    Add Repositories and Services component

---
+++ src/components/user/Repositories.js
@@ -0,0 +1,15 @@
+import React, { PropTypes } from 'react';
+import { Link } from 'react-router';
+
+const Repositories = () => {
+  return (
+    <div>
+      <h3>Repositories</h3>
+      <div className="row">
+        <Link to="#" className="medium-2 columns button"><span className="fi-social-github">Github</span></Link>
+      </div>
+    </div>
+  );
+};
+
+export default Repositories;
