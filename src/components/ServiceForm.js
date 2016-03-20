c3bdf3720cca618a704be8aa9daa889101ca9bf0
Leonel Perea <iticleonel.len@gmail.com>
2016-03-20 13:14:27 -0600

    issue #14 create footer component

--- src/components/ServiceForm.js
+++ src/components/ServiceForm.js
@@ -13,20 +13,31 @@ import ServiceSummary from './ServiceSummary';
 const ServiceForm = () => {
     return (
       <div className="row">
-        <div className="large-10 columns">
-            <h1><i className="step fi-clipboard-notes"></i> Create a Service</h1>
-            <ProjectName />
-            <AutomationSoftware />
-            <ServerProvider />
-            <Distribution />
-            <Application />
-            <Package />
-            <SSHKeys />
-            <CreateService />
-        </div>
-        <div className="large-2 columns hide-for-small-only hide-for-medium-only">
-            <ServiceSummary />
-        </div>
+          <div className="row">
+            <div className="large-10 columns">
+                <h1><i className="step fi-clipboard-notes"></i> Create a Service</h1>
+                <ProjectName />
+                <AutomationSoftware />
+                <ServerProvider />
+                <Distribution />
+                <Application />
+                <Package />
+                <SSHKeys />
+                <CreateService />
+            </div>
+            <div className="large-2 columns hide-for-small-only hide-for-medium-only">
+                <ServiceSummary />
+            </div>
+          </div>
+          <div className="row">
+              <footer>
+                <div className="row">
+                  <div className="large-12 large-centered medium-12 medium-centered small-12 small-centered columns">
+                    <p className="copyright">© 2015, Inc. All rights reserved.</p>
+                  </div>
+                </div>
+              </footer>
+          </div>
       </div>
     );
 };
