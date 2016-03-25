3afe40023e3f30634d59aab56c85bc9b4ed6909f
Leonel Perea <iticleonel.len@gmail.com>
2016-03-25 13:03:55 -0600

    issue #43 create automation software reducer

---
+++ src/test/automationSoftware.spec.js
@@ -0,0 +1,16 @@
+import {Map, fromJS} from 'immutable';
+import { expect } from 'chai';
+
+import automationSoftware from '../reducers/automationSoftware';
+
+describe('reducer', () => {
+  it('handles SET_AUTOMATION_SOFTWARE', () => {
+    const initialState = Map();
+    const action = {type: 'SET_AUTOMATION_SOFTWARE', value: 'ansible'};
+    const nextState = automationSoftware(initialState, action);
+
+    expect(nextState).to.equal(fromJS({
+      automation_software: 'ansible'
+    }));
+  });
+});
