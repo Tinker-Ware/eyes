c1c87ed7c1886956bdc02b7ed305ee4601a6aab1
Leonel Perea <iticleonel.len@gmail.com>
2016-03-28 17:57:01 -0600

    issue #50 add redux provider and rename provision reducer to projecName

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import {render} from 'react-dom';
+import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
+import configureStore from './store/configureStore';
 import './styles/foundation.css';
 import './styles/styles.scss';
 import './icons/foundation-icons.css';
 
+const store = configureStore();
+
 render(
-  <Router history={browserHistory} routes={routes} />, document.getElementById('app')
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>, document.getElementById('app')
 );
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import projectNameAppState from './projectName';
+
+const rootReducer = combineReducers({
+  projectNameAppState
+});
+
+export default rootReducer;
