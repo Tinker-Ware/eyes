9332736472b4335fb39f1acee9599812613f2bfb
Leonel Perea <iticleonel.len@gmail.com>
2016-04-20 07:58:49 -0500

    issue #72 enable or disable button create service

--- src/components/provisionerForm/CreateService.js
+++ src/components/provisionerForm/CreateService.js
@@ -1,12 +1,24 @@
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
 
-const CreateService = () => {
+const CreateService = ( {projectNameAppState, repositoryAppState, applicationAppState} ) => {
+    const btnCreateService = repositoryAppState.get('user_name') && 
+                    repositoryAppState.get('repository_name') &&
+                    projectNameAppState.get('project_name') && 
+                    applicationAppState.get('application_name') ? 
+                    <a href="javascript:void(0);" id="btn-create_service" className="button radius expanded"><i className="step fi-power"></i> Create Service</a> : 
+                    <a href="javascript:void(0);" id="btn-create_service" className="button radius expanded disabled"><i className="step fi-power"></i> Create Service</a>;
     return (
         <div className="large-12 large-centered medium-12 medium-centered small-12 small-centered columns">
-          <a href="javascript:void(0);" id="btn-create_service" className="button radius expanded disabled"><i className="step fi-power"></i> Create Service</a>
+          {btnCreateService}
       </div>
     );
+};
+
+CreateService.propTypes = {
+    projectNameAppState: PropTypes.object.isRequired,
+    repositoryAppState: PropTypes.object.isRequired,
+    applicationAppState: PropTypes.object.isRequired
 };
 
 export default CreateService;
