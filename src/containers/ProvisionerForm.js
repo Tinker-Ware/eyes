10184520803fac69e30681a0b483de66f5b66241
Leonel Perea <iticleonel.len@gmail.com>
2016-04-28 22:40:26 -0500

    Each child in an array or iterator should have a unique key prop

--- src/containers/ProvisionerForm.js
+++ src/containers/ProvisionerForm.js
@@ -31,6 +31,7 @@ export class ServiceForm extends Component {
                       setProjectName={this.props.actions.setProjectName} />
                     {(provisionFormOptionsApi.getProvisionFormOptions()[0])? provisionFormOptionsApi.getProvisionFormOptions()[0].services.map((value, index) => (value.identifier == 'github') ?
                       <GithubService 
+                        key = {value.identifier}
                         repositoryAppState={this.props.repositoryAppState}
                         setGitHubUserName={this.props.actions.setGitHubUserName}
                         setGitHubRepositoryName={this.props.actions.setGitHubRepositoryName}
