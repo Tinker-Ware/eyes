1e871d25a5737c6cefc304acb8cf0562626486be
Leonel Perea <iticleonel.len@gmail.com>
2016-05-25 17:57:10 -0500

    Added .scss as excluded feature for tests

--- tools/testSetup.js
+++ tools/testSetup.js
@@ -11,15 +11,9 @@ process.env.NODE_ENV = 'production';
 
 // Disable webpack-specific features for tests since
 // Mocha doesn't know what to do with them.
-require.extensions['.css'] = function () {
-  return null;
-};
-require.extensions['.png'] = function () {
-  return null;
-};
-require.extensions['.jpg'] = function () {
-  return null;
-};
+['.css', '.scss', '.png', '.jpg'].forEach(ext => {
+  require.extensions[ext] = () => null;
+});
 
 // Register babel so that it will transpile ES6 to ES5
 // before our tests run.
