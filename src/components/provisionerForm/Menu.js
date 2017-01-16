e5835df4b625d57ec1ccf86804a809f8a9ff6031
ileonelperea <leonel@tinkerware.io>
2017-01-15 23:24:54 -0600

    Update Menu to materialDesign

--- src/components/provisionerForm/Menu.js
+++ src/components/provisionerForm/Menu.js
@@ -1,37 +1,95 @@
-import React, { PropTypes } from "react";
-import { Link } from "react-router";
+import {Card} from "material-ui/Card";
+import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from "material-ui/Toolbar";
+import AppBar from "material-ui/AppBar";
+import FontIcon from "material-ui/FontIcon";
+import IconButton from "material-ui/IconButton";
+import IconMenu from "material-ui/IconMenu";
+import MenuItem from "material-ui/MenuItem";
+import MoreVertIcon from "material-ui/svg-icons/navigation/more-vert";
+import RaisedButton from "material-ui/RaisedButton";
+import React, {PropTypes} from "react";
+
+const style = {
+  button: {
+    margin: 12,
+  },
+  toolbar: {
+    margin: "-1em -1em 3em -1em",
+  },
+  toolbarTitle: {
+    marginLeft: "1em"
+  }
+};
 
 const Menu = ( { userAppState } ) => {
   return (
-    <div data-magellan-expedition="fixed">
-      <div className="row">
-        <dl className="sub-nav">
-          <dd className="active">
-            <a href="#project-configuration">
-              <i className="step fi-wrench" />
-              {"Project Configuration"}
-            </a>
-          </dd>
-          <dd>
-            <a href="#connect-service">
-              <i className="step fi-share" />
-              {"Connect Service"}
-            </a>
-          </dd>
-          <dd>
-            <a href="#aplications">
-              <i className="step fi-social-dropbox" />
-              {"Aplications"}
-            </a>
-          </dd>
-          <dd className="user-login">
-            <Link to="/user">
-              <i className="step fi-torso" />
-              &nbsp;{"Logged as"} {userAppState.get("user_session")?userAppState.get("user_session").toJS().email:""}
-            </Link>
-          </dd>
-        </dl>
-      </div>
+    <div className="card">
+      <AppBar
+          iconElementLeft={
+            <IconButton>
+              <FontIcon className="icon icon-home"/>
+            </IconButton>
+          }
+          iconElementRight={
+            <IconMenu
+                anchorOrigin={{
+                  horizontal: "right",
+                  vertical: "top"
+                }}
+                iconButtonElement={
+                  <IconButton><MoreVertIcon /></IconButton>
+                }
+                targetOrigin={{
+                  horizontal: "right",
+                  vertical: "top"
+                }}
+            >
+              <MenuItem
+                  href="/user"
+                  primaryText="User Information"
+              />
+              <MenuItem primaryText="Help" />
+              <MenuItem primaryText="Sign out" />
+            </IconMenu>
+          }
+          title="My DevOp"
+      />
+      <Card>
+        <Toolbar style={style.toolbar}>
+          <ToolbarGroup firstChild>
+            <FontIcon className="icon icon-project"/>
+            <ToolbarTitle
+                style={style.toolbarTitle}
+                text="Create New Project"
+            />
+          </ToolbarGroup>
+          <ToolbarGroup>
+            <ToolbarTitle text="Options" />
+            <ToolbarSeparator />
+            <RaisedButton
+                href={"#"}
+                icon={<FontIcon className="icon icon-edit" />}
+                label={"Edit"}
+                primary
+                style={style.button}
+            />
+            <RaisedButton
+                href={"#"}
+                icon={<FontIcon className="icon icon-deploy" />}
+                label={"Deploy"}
+                primary
+                style={style.button}
+            />
+            <RaisedButton
+                href={"#"}
+                icon={<FontIcon className="icon icon-cloud-download" />}
+                label={"Download"}
+                primary
+                style={style.button}
+            />
+          </ToolbarGroup>
+        </Toolbar>
+      </Card>
     </div>
   );
 };
--- src/containers/ProvisionerForm.js
+++ src/containers/ProvisionerForm.js
@@ -1,16 +1,17 @@
-import React, { Component, PropTypes } from "react";
-import { connect } from "react-redux";
-import { browserHistory } from "react-router";
 import { bindActionCreators } from "redux";
+import { browserHistory } from "react-router";
+import { connect } from "react-redux";
 import { fromJS } from "immutable";
-import cookie from "react-cookie";
 import * as actions from "../actions/ServiceFormActions";
 import Application from "../components/provisionerForm/Application";
-import CreateService from "../components/provisionerForm/CreateService";
 import CloudProvider from "../components/provisionerForm/CloudProvider";
+import cookie from "react-cookie";
+import CreateService from "../components/provisionerForm/CreateService";
 import GithubService from "../components/provisionerForm/GithubService";
 import Menu from "../components/provisionerForm/Menu";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import ProjectName from "../components/provisionerForm/ProjectName";
+import React, { Component, PropTypes } from "react";
 import ServiceSummary from "../components/provisionerForm/ServiceSummary";
 import SSHKeys from "../components/provisionerForm/SSHKeys";
 
@@ -29,93 +30,95 @@ export class ServiceForm extends Component {
       }));
     }
     return (
-      <div className="row">
-        <Menu userAppState={this.props.userAppState} />
-        <div className="row">
-          <div className="large-10 columns">
-            <h1><i className="step fi-clipboard-notes" />
-              {"Create a Service"}
-            </h1>
-            <ProjectName
-                projectNameAppState={this.props.projectNameAppState}
-                setProjectName={this.props.actions.setProjectName}
-            />
-            <div className="row">
-              <h2>
-                <i className="step fi-share" />
-                {"Connect Service(s)"}
-              </h2>
-              {(provisionFormOptionsApi.getProvisionFormOptions()[0]) ?
-                provisionFormOptionsApi.getProvisionFormOptions()[0].services.map((value) =>
-                  (value.identifier =="github") ?
-                    <GithubService
-                        key = {value.identifier}
-                        repositoryAppState={this.props.repositoryAppState}
-                        requestRepositoryAccess={this.props.actions.requestRepositoryAccess}
-                        requestUserRepositories={this.props.actions.requestUserRepositories}
-                        setIntegracion={this.props.actions.setIntegracion}
-                        setRepository={this.props.actions.setRepository}
-                        setShowRepositories={this.props.actions.setShowRepositories}
-                        userAppState={this.props.userAppState}
-                    />
-                  :"")
-                :""}
-              <CloudProvider
-                  clearCloudProviderSSHKeys={this.props.actions.clearCloudProviderSSHKeys}
-                  cloudProviderAppState={this.props.cloudProviderAppState}
-                  requestCloudProviderAccess={this.props.actions.requestCloudProviderAccess}
-                  setCloudProvider={this.props.actions.setCloudProvider}
-                  userAppState={this.props.userAppState}
-              />
-            </div>
-            <Application
-                applicationAppState={this.props.applicationAppState}
-                applicationsOptions={(provisionFormOptionsApi.getProvisionFormOptions()[0]) ?  provisionFormOptionsApi.getProvisionFormOptions()[0].application :""}
-                setApplication={this.props.actions.setApplication}
-            />
-            <SSHKeys
-                cloudProviderAppState={this.props.cloudProviderAppState}
-                deleteSSHKey={this.props.actions.deleteSSHKey}
-                enableSSHKey={this.props.actions.enableSSHKey}
-                requestPostCloudProviderSSHKey={this.props.actions.requestPostCloudProviderSSHKey}
-                setSSHKey={this.props.actions.setSSHKey}
-                setSSHKeyContent={this.props.actions.setSSHKeyContent}
-                setSSHKeyTitle={this.props.actions.setSSHKeyTitle}
-                showSSHKey={this.props.actions.showSSHKey}
-                userAppState={this.props.userAppState}
-            />
-            <CreateService
-                applicationAppState={this.props.applicationAppState}
-                cloudProviderAppState={this.props.cloudProviderAppState}
-                projectNameAppState={this.props.projectNameAppState}
-                repositoryAppState={this.props.repositoryAppState}
-                requestPostUserProject={this.props.actions.requestPostUserProject}
-                userAppState={this.props.userAppState}
-            />
-          </div>
-          <div className="large-2 columns hide-for-small-only hide-for-medium-only">
-            <ServiceSummary
-                applicationAppState={this.props.applicationAppState}
-                cloudProviderAppState={this.props.cloudProviderAppState}
-                projectNameAppState={this.props.projectNameAppState}
-                repositoryAppState={this.props.repositoryAppState}
-                requestPostUserProject={this.props.actions.requestPostUserProject}
-                userAppState={this.props.userAppState}
-            />
-          </div>
-        </div>
-        <div className="row">
-          <footer>
-            <div className="row">
-              <div className="large-12 large-centered medium-12 medium-centered small-12 small-centered columns">
-                <p className="copyright">
-                  {"© 2015, Inc. All rights reserved."}
-                </p>
+      <MuiThemeProvider>
+        <div className="small-12 medium-12 large-12 large-centered columns">
+            <Menu userAppState={this.props.userAppState} />
+            {/* <div className="row">
+              <div className="large-10 columns">
+                <h1><i className="step fi-clipboard-notes" />
+                  {"Create a Service"}
+                </h1>
+                <ProjectName
+                    projectNameAppState={this.props.projectNameAppState}
+                    setProjectName={this.props.actions.setProjectName}
+                />
+                <div className="row">
+                  <h2>
+                    <i className="step fi-share" />
+                    {"Connect Service(s)"}
+                  </h2>
+                  {(provisionFormOptionsApi.getProvisionFormOptions()[0]) ?
+                    provisionFormOptionsApi.getProvisionFormOptions()[0].services.map((value) =>
+                      (value.identifier =="github") ?
+                        <GithubService
+                            key = {value.identifier}
+                            repositoryAppState={this.props.repositoryAppState}
+                            requestRepositoryAccess={this.props.actions.requestRepositoryAccess}
+                            requestUserRepositories={this.props.actions.requestUserRepositories}
+                            setIntegracion={this.props.actions.setIntegracion}
+                            setRepository={this.props.actions.setRepository}
+                            setShowRepositories={this.props.actions.setShowRepositories}
+                            userAppState={this.props.userAppState}
+                        />
+                      :"")
+                    :""}
+                  <CloudProvider
+                      clearCloudProviderSSHKeys={this.props.actions.clearCloudProviderSSHKeys}
+                      cloudProviderAppState={this.props.cloudProviderAppState}
+                      requestCloudProviderAccess={this.props.actions.requestCloudProviderAccess}
+                      setCloudProvider={this.props.actions.setCloudProvider}
+                      userAppState={this.props.userAppState}
+                  />
+                </div>
+                <Application
+                    applicationAppState={this.props.applicationAppState}
+                    applicationsOptions={(provisionFormOptionsApi.getProvisionFormOptions()[0]) ?  provisionFormOptionsApi.getProvisionFormOptions()[0].application :""}
+                    setApplication={this.props.actions.setApplication}
+                />
+                <SSHKeys
+                    cloudProviderAppState={this.props.cloudProviderAppState}
+                    deleteSSHKey={this.props.actions.deleteSSHKey}
+                    enableSSHKey={this.props.actions.enableSSHKey}
+                    requestPostCloudProviderSSHKey={this.props.actions.requestPostCloudProviderSSHKey}
+                    setSSHKey={this.props.actions.setSSHKey}
+                    setSSHKeyContent={this.props.actions.setSSHKeyContent}
+                    setSSHKeyTitle={this.props.actions.setSSHKeyTitle}
+                    showSSHKey={this.props.actions.showSSHKey}
+                    userAppState={this.props.userAppState}
+                />
+                <CreateService
+                    applicationAppState={this.props.applicationAppState}
+                    cloudProviderAppState={this.props.cloudProviderAppState}
+                    projectNameAppState={this.props.projectNameAppState}
+                    repositoryAppState={this.props.repositoryAppState}
+                    requestPostUserProject={this.props.actions.requestPostUserProject}
+                    userAppState={this.props.userAppState}
+                />
               </div>
-            </div>
-          </footer>
+              <div className="large-2 columns hide-for-small-only hide-for-medium-only">
+                <ServiceSummary
+                    applicationAppState={this.props.applicationAppState}
+                    cloudProviderAppState={this.props.cloudProviderAppState}
+                    projectNameAppState={this.props.projectNameAppState}
+                    repositoryAppState={this.props.repositoryAppState}
+                    requestPostUserProject={this.props.actions.requestPostUserProject}
+                    userAppState={this.props.userAppState}
+                />
+              </div>
+            </div> */}
+            {/* <div className="row">
+              <footer>
+                <div className="row">
+                  <div className="large-12 large-centered medium-12 medium-centered small-12 small-centered columns">
+                    <p className="copyright">
+                      {"© 2015, Inc. All rights reserved."}
+                    </p>
+                  </div>
+                </div>
+              </footer>
+            </div> */}
         </div>
-      </div>
+      </MuiThemeProvider>
     );
   }
 }
