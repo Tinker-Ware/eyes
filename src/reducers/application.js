34e7ea76f0e7b2d4795a03ac8c5aacc575432324
ileonelperea <leonel@tinkerware.io>
2017-01-22 09:19:47 -0600

    Test apps

--- src/reducers/application.js
+++ src/reducers/application.js
@@ -1,4 +1,4 @@
-import { SET_APPLICATION } from "../constants/ActionTypes";
+import { SET_APPLICATION, SET_APPLICATION_ONE_CLICK_APP } from "../constants/ActionTypes";
 import { Map } from "immutable";
 
 const initialState = Map({
@@ -10,6 +10,16 @@ export default function application(state = initialState, action) {
     case SET_APPLICATION:
     {
       return state.set("application_name", action.value.get("application"));
+    }
+    case SET_APPLICATION_ONE_CLICK_APP:
+    {
+      return state.set("application_oneclickapp",
+        action.value.get("applications").toSet().union(
+          action.value.get("application").map( value =>
+            value.set("enable", false)
+          )
+        ).toList()
+      );
     }
     default:
       return state;
